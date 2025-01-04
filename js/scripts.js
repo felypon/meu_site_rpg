59e16113db2a163bf01696ed8d45d9a8017d6c2a
felypon <129194302+felypon@users.noreply.github.com>
2025-01-04 16:52:26 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+document.getElementById("formCadastro").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Cadastro realizado com sucesso!");
+});
+
+document.getElementById("formLogin").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Login realizado com sucesso!");
+});
